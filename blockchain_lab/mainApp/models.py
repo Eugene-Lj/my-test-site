4198d1e48071bac757f5d4b401252361f36f25f3
Eugene-Lj <leznev.zheka12391@gmail.com>
2020-06-23 23:51:11 +0600

    add forloop for about

--- blockchain_lab/mainApp/models.py
+++ blockchain_lab/mainApp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class SERVICES(models.Model):
     s_title = models.CharField(max_length=200)
     s_post = models.TextField()
-    s_image = models.ImageField(null=True, upload_to='images/')
+    #s_image = models.ImageField(null=True, upload_to='images/')
 
     def __str__(self):
         return self.s_title
