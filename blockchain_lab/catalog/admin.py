9230582b48da2d49ecc5921c4bfb8fc90049285c
Eugene-Lj <leznev.zheka12391@gmail.com>
2020-06-27 01:51:05 +0600

    добалена таблица в html, рабочая админка

--- blockchain_lab/catalog/admin.py
+++ blockchain_lab/catalog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Table
+from .forms import FileFieldForm
 
 class TableAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Table, TableAdmin)
+'''class TableFileInline(admin.TabularInline):
+    model = TableFile
+
+class TableAdmin(admin.ModelAdmin): inlines = [TableFileInline,]'''
--- blockchain_lab/catalog/models.py
+++ blockchain_lab/catalog/models.py
@@ -5,7 +5,12 @@ class Table(models.Model):
     end_date = models.DateTimeField(null=True)
     title = models.CharField(max_length=500)
     description = models.TextField()
-    #files = models.FileField(upload_to='uploads/', null=True)
-
+    files = models.FileField(upload_to='uploads/', null=True)
+    files2 = models.FileField(upload_to='uploads/', null=True)
+    
     def __str__(self):
-        return self.title 
+        return self.title
+
+'''class TableFile(models.Model):
+    files = models.FileField(upload_to='uploads/', null=True)
+    page = models.ForeignKey('Table', on_delete=models.DO_NOTHING)'''
